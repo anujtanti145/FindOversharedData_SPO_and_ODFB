cef89ae54bd0a7282a110f31334995e7b4a9d14c
Anuj Tanti <antanti@microsoft.com>
2024-10-15 14:11:47 +1100

    Script changes

--- Oversharing/GenerateOversharedDataReportV3.ps1
+++ Oversharing/GenerateOversharedDataReportV3.ps1
@@ -1,8 +1,5 @@
-﻿
-# Copyright (C) Microsoft Corporation.  All rights reserved.
-# DiSCLAIMER:
-# Copyright (c) Microsoft Corporation. All rights reserved. This
-# script is made available to you without any express, implied or
+﻿# DiSCLAIMER:
+# This script is made available to you without any express, implied or
 # statutory warranty, not even the implied warranty of
 # merchantability or fitness for a particular purpose, or the
 # warranty of title or non-infringement. The entire risk of the
